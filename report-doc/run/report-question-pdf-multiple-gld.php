4b4e3a01b5f89004ef4d8a41c2e55e6019ed309a
amitpandey24 <amitpandey.his@gmail.com>
2024-01-04 09:29:51 +0530

    changes in survey report title

--- report-doc/run/report-question-pdf-multiple-gld.php
+++ report-doc/run/report-question-pdf-multiple-gld.php
@@ -138,7 +138,7 @@ $html = '<!DOCTYPE html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Survey Report Question</title>
         <style>
             table {
                 font-family: arial, sans-serif;
